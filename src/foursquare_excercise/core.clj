197f62cb77ebcd28e516b400c81393e9d4c702bb
iecya <federica.delisi@hotmail.it>
2019-02-24 17:22:41 +0000

    Add exit condition

--- src/foursquare_excercise/core.clj
+++ src/foursquare_excercise/core.clj
@@ -7,5 +7,8 @@
   (println "// ----- Welcome to Foursquare API excercise ----- \\\\")
   (println "Please input a place to search or type 'exit' to stop the app")
   (loop [*in* (read-line)]
-        (println "this is my input: " *in*)
-        (recur (read-line))))
+        (if (= "exit" *in*)
+          (println "good bye")
+          (do
+            (println "This is my input: " *in*)
+            (recur (read-line))))))
