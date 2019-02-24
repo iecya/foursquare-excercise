0c26da60480afae2f78e33d954118790051164dc
iecya <federica.delisi@hotmail.it>
2019-02-24 17:51:30 +0000

    Get venues for searched place

--- src/foursquare_excercise/core.clj
+++ src/foursquare_excercise/core.clj
@@ -1,5 +1,24 @@
 (ns foursquare-excercise.core
+  (:require [org.httpkit.client :as http])
     (:gen-class))
+
+(def ^:private app-config {:client_id "OZE3X22Y4Z41PHWBUAFRU1RIUVS4Q3WJCXC1ZEDLNQOMBJ1I"
+                           :client_secret "EYCJ1GNSXPLD4GHRUVLDTIIK5MCB5WAV1OJZQGH1ABHY3UAN"})
+
+(def default-opts
+  (merge app-config
+         {:intent "global"
+          :v "20190224"}))
+
+(defn- get-recomendetions
+  [input]
+  (let [
+        searched-place (http/get "https://api.foursquare.com/v2/venues/search"
+                                 {:query-params (merge default-opts
+                                                       {:query input})})]
+    (println "Result " @searched-place)
+    @searched-place))
+
 
 (defn -main
   "I don't do a whole lot."
@@ -10,5 +29,5 @@
         (if (= "exit" *in*)
           (println "good bye")
           (do
-            (println "This is my input: " *in*)
+            (get-recomendetions *in*)
             (recur (read-line))))))
