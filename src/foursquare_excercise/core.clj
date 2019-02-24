7a575ff852519f7074a71c523213a5531662c891
iecya <federica.delisi@hotmail.it>
2019-02-24 18:53:54 +0000

    Use searched place coords to find nearby recomended venues

--- src/foursquare_excercise/core.clj
+++ src/foursquare_excercise/core.clj
@@ -4,28 +4,35 @@
     (:gen-class))
 
 (def ^:private app-config {:client_id "OZE3X22Y4Z41PHWBUAFRU1RIUVS4Q3WJCXC1ZEDLNQOMBJ1I"
-                           :client_secret "EYCJ1GNSXPLD4GHRUVLDTIIK5MCB5WAV1OJZQGH1ABHY3UAN"})
+                           :client_secret "EYCJ1GNSXPLD4GHRUVLDTIIK5MCB5WAV1OJZQGH1ABHY3UAN"
+                           :v "20190224"})
 
 (def default-opts
   (merge app-config
-         {
-          ;:intent "global"
-          :v "20190224"
-          }))
+         {:intent "global"}))
 
 (defn- get-searched-venue
   [input]
-  (let [searched-place (http/get "https://api.foursquare.com/v2/venues/explore"
+  (let [searched-place (http/get "https://api.foursquare.com/v2/venues/search"
                                  {:query-params (merge default-opts
-                                                       {:near input
-                                                        :query input})})]
-    (clojure.pprint/pprint @searched-place)
-    (clojure.pprint/pprint (-> @searched-place :body (json/read-str :key-fn keyword) :response :groups :items))
+                                                       {:query input})})]
     (-> @searched-place :body (json/read-str :key-fn keyword) :response :venues first)))
+
+(defn- get-recomended-venues
+  [{:keys [location]}]
+  (let [api-resp (http/get "https://api.foursquare.com/v2/venues/search"
+                           {:query-params (merge app-config
+                                                 {:ll (str (:lat location) "," (:lng location))})})]
+    (println "Api resp for recomendations <<<<<<")
+    (clojure.pprint/pprint (-> @api-resp :body (json/read-str :key-fn keyword) :response :venues))
+    api-resp))
 
 (defn- get-recomendetions
   [input]
   (let [searched-place (get-searched-venue input)]
+    (println "Searched place >>>>>")
+    (clojure.pprint/pprint searched-place)
+    (get-recomended-venues searched-place)
     searched-place))
 
 
@@ -38,5 +45,5 @@
         (if (= "exit" *in*)
           (println "good bye")
           (do
-            (println (get-recomendetions *in*))
+            (get-recomendetions *in*)
             (recur (read-line))))))
